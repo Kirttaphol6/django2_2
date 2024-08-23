0852af6d348baaf6350e8971bc2248df8557a34b
Kirttaphol <kirttaphol1@gmail.com>
2024-08-23 16:47:21 +0700

    first commit

---
+++ project1/members/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+def members(request):
+    template = loader.get_template('myfirst.html')
+    return HttpResponse("")
+
+
+def profile(request):
+    template = loader.get_template('profile.html')
+    return HttpResponse("")
+
+
+def school(request):
+    template = loader.get_template('myfirst.html')
+    return HttpResponse("")
+
